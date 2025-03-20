7d25db8634ab6e1761ecc46c96d471f8a29884fe
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-03-20 11:31:40 -0700

    add item menu

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+const MenuItem = ({ menu, index }) => {
+  return (
+    <a
+      href={menu.uri}
+      className="w-12 h-12 rounded-full flex items-center
+       justify-center group cursor-pointer 
+       hover:bg-gradient-to-br 
+       hover:from-primary hover:to-secondary
+       "
+    >
+      <menu.Icon className={`text-texlight text-xl`} />
+    </a>
+  );
 };
 
 export default MenuItem;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Header } from "./";
 
 const App = () => {
   return (
-    <div className="flex item-center justify-center">
-      <h1>Hi there</h1>
+    <div className="w-full xl:w-[1600px] py:32 px-4 lg:px-12 pr-4 lg:pr-32">
+      {/* particals container*/}
+
+      {/* header */}
+      <Header />
+
+      {/* home contaier */}
+
+      {/* about contaier */}
+
+      {/* skills contaier */}
+
+      {/* projects contaier */}
+
+      {/* contact contaier */}
+
+      {/* footer contaier */}
     </div>
   );
 };
