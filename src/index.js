cb35d83b09b844110ea9daa662c4938d970aafed
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-06-21 12:54:19 -0700

    Add analytics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./containers";
+import { Analytics } from "@vercel/analytics/react"; // Add this import
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics /> {/* Add this line */}
   </React.StrictMode>
 );
